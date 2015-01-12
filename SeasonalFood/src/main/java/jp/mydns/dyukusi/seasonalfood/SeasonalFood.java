daa85520bc03689f7858176a05e1d9a5f592d0e7
Dyukusi <dyrmc694@yahoo.co.jp>
2015-01-13 07:49:41 +0900

    support pumpkin and melon failure

--- SeasonalFood/src/main/java/jp/mydns/dyukusi/seasonalfood/SeasonalFood.java
+++ SeasonalFood/src/main/java/jp/mydns/dyukusi/seasonalfood/SeasonalFood.java
@@ -1,9 +1,11 @@
 package jp.mydns.dyukusi.seasonalfood;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 
 import jp.mydns.dyukusi.seasonalfood.command.BasicCommand;
+import jp.mydns.dyukusi.seasonalfood.listener.BarrenDirt;
 import jp.mydns.dyukusi.seasonalfood.listener.GrowEvent;
 import jp.mydns.dyukusi.seasonalfood.listener.PlayerJoin;
 import jp.mydns.dyukusi.seasonalfood.seasontype.SeasonType;
@@ -11,14 +13,17 @@ import jp.mydns.dyukusi.seasonalfood.task.SeasonChanger;
 import jp.mydns.dyukusi.title.Title;
 
 import org.bukkit.ChatColor;
+import org.bukkit.CropState;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SeasonalFood extends JavaPlugin {
 
-	private String prefix = ChatColor.YELLOW + "[SeasonalFood]" + ChatColor.WHITE;
+	private String prefix = ChatColor.YELLOW + "[SeasonalFood]"
+			+ ChatColor.WHITE;
 	private HashMap<Material, Double[]> ripe_rate_map;
 	private SeasonType current_season;
 	private boolean force_next_season;
@@ -30,6 +35,13 @@ public class SeasonalFood extends JavaPlugin {
 		this.current_season = SeasonType.SPRING;
 		this.force_next_season = false;
 
+		// config test
+		if (!new File(this.getDataFolder().getAbsolutePath() + "/config.yml")
+				.exists()) {
+			getLogger().info("config.yml doesn't exist. creating...");
+			this.saveDefaultConfig();
+		}
+
 		List<String> list = this.getConfig().getStringList("ripe_rate");
 		for (String str : list) {
 			String[] array = str.split(",");
@@ -38,16 +50,20 @@ public class SeasonalFood extends JavaPlugin {
 			double summer_rate = (double) Integer.parseInt(array[2]) / 100.0;
 			double autumn_rate = (double) Integer.parseInt(array[3]) / 100.0;
 			double winter_rate = (double) Integer.parseInt(array[4]) / 100.0;
-			Double[] rate = { spring_rate, summer_rate, autumn_rate, winter_rate };
+			Double[] rate = { spring_rate, summer_rate, autumn_rate,
+					winter_rate };
 
 			this.ripe_rate_map.put(crops, rate);
 		}
 
 		// register listener
-		this.getServer().getPluginManager().registerEvents(new GrowEvent(this), this);
-		this.getServer().getPluginManager().registerEvents(new PlayerJoin(this), this);
+		this.getServer().getPluginManager()
+				.registerEvents(new GrowEvent(this), this);
+		this.getServer().getPluginManager()
+				.registerEvents(new PlayerJoin(this), this);
+		this.getServer().getPluginManager()
+				.registerEvents(new BarrenDirt(this), this);
 
-		
 		// run season check task
 		new SeasonChanger(this).runTaskTimer(this, 10L, 50L);
 
@@ -88,9 +104,22 @@ public class SeasonalFood extends JavaPlugin {
 		return this.ripe_rate_map.containsKey(material);
 	}
 
-	public void display_season(boolean broadcast, Player player, SeasonType season) {
+	public void set_dirt_from_soil(Block block) {
 
-		Title change_season = new Title(season.get_inJapanese(), "< " + season.toString() + " >");
+		// is there dirt at beneath the block
+		Block soil = block.getWorld().getBlockAt(
+				block.getLocation().add(0, -1, 0));
+		if (soil.getType().equals(Material.SOIL)) {
+			soil.setType(Material.DIRT);
+		}
+
+	}
+
+	public void display_season(boolean broadcast, Player player,
+			SeasonType season) {
+
+		Title change_season = new Title(season.get_inJapanese(), "< "
+				+ season.toString() + " >");
 		change_season.setTitleColor(season.get_season_color());
 		change_season.setSubtitleColor(ChatColor.AQUA);
 
@@ -105,7 +134,8 @@ public class SeasonalFood extends JavaPlugin {
 		// to player
 		else {
 			change_season.send(player);
-			player.playSound(player.getLocation(), Sound.AMBIENCE_CAVE, 1.5F, 0.8F);
+			player.playSound(player.getLocation(), Sound.AMBIENCE_CAVE, 1.5F,
+					0.8F);
 		}
 
 	}
--- SeasonalFood/src/main/java/jp/mydns/dyukusi/seasonalfood/listener/GrowEvent.java
+++ SeasonalFood/src/main/java/jp/mydns/dyukusi/seasonalfood/listener/GrowEvent.java
@@ -1,6 +1,7 @@
 package jp.mydns.dyukusi.seasonalfood.listener;
 
 import jp.mydns.dyukusi.seasonalfood.SeasonalFood;
+import jp.mydns.dyukusi.seasonalfood.task.BreakStem;
 import jp.mydns.dyukusi.seasonalfood.task.Soulsand_to_dirt;
 
 import org.bukkit.ChatColor;
@@ -27,11 +28,23 @@ public class GrowEvent implements Listener {
 	@EventHandler
 	void BlockGrow(BlockGrowEvent event) {
 		Material type = event.getBlock().getType();
-		boolean isRipe = false;	
+		boolean isRipe = false;
 
+		// Melon , Pumpkin
+		if (type.equals(Material.AIR)) {
+			new BreakStem(plugin, event.getBlock())
+					.runTaskAsynchronously(plugin);
+			return;
+		}
+		// Melon_stem , Pumpkin_stem
+		else if (type.equals(Material.PUMPKIN_STEM)
+				|| type.equals(Material.PUMPKIN_STEM)) {
+			return;
+		}
 		// NetherWarts
-		if (type.equals(Material.NETHER_WARTS)) {
-			if (NetherWartsState.RIPE.equals(NetherWartsState.values()[event.getNewState().getData().getData()])) {
+		else if (type.equals(Material.NETHER_WARTS)) {
+			if (NetherWartsState.RIPE.equals(NetherWartsState.values()[event
+					.getNewState().getData().getData()])) {
 				isRipe = true;
 			}
 		}
@@ -44,7 +57,8 @@ public class GrowEvent implements Listener {
 		}
 		// Crops
 		else {
-			if (CropState.RIPE.equals(CropState.getByData(event.getNewState().getData().getData()))) {
+			if (CropState.RIPE.equals(CropState.getByData(event.getNewState()
+					.getData().getData()))) {
 				isRipe = true;
 			}
 		}
@@ -52,7 +66,8 @@ public class GrowEvent implements Listener {
 		if (isRipe) {
 			Block block = event.getBlock();
 			if (plugin.get_isContain_ripe_rate(type)) {
-				double rate = plugin.get_ripe_rate(type, plugin.get_current_season());
+				double rate = plugin.get_ripe_rate(type,
+						plugin.get_current_season());
 				double random = Math.random();
 				// plugin.getServer().broadcastMessage(random + "  vs  " +
 				// rate);
@@ -63,17 +78,26 @@ public class GrowEvent implements Listener {
 
 					// SoulSand beneath NetherWarts becomes dirt when it ripes.
 					if (type.equals(Material.NETHER_WARTS)) {
-						new Soulsand_to_dirt(plugin, block).runTaskLater(plugin, 20L);
+						new Soulsand_to_dirt(plugin, block).runTaskLater(
+								plugin, 20L);
 					}
+					// normal crops
+					else if (Material.CROPS.equals(type)
+							|| Material.POTATO.equals(type)
+							|| Material.CARROT.equals(type)) {
+						plugin.set_dirt_from_soil(block);
+					}
+
 				}
 
 			} else {
-				plugin.getServer().broadcastMessage(plugin.get_prefix() + ChatColor.RED + " " + "is not found!");
+				plugin.getServer().broadcastMessage(
+						plugin.get_prefix() + ChatColor.RED + type + " "
+								+ "is not found!");
 			}
 
 		}
 
 	}
-
 
 }
