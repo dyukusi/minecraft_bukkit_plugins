600169831093f3599a1e0b3adb1ea6a9b510843a
Dyukusi <dyrmc694@yahoo.co.jp>
2015-01-16 02:57:12 +0900

    add lights forbid under y30 function

--- MyPlugin/src/main/java/jp/mydns/dyukusi/myplugin/MyPlugin.java
+++ MyPlugin/src/main/java/jp/mydns/dyukusi/myplugin/MyPlugin.java
@@ -1,6 +1,9 @@
 package jp.mydns.dyukusi.myplugin;
 
 import jp.mydns.dyukusi.myplugin.listener.PlayerEffect;
+import jp.mydns.dyukusi.myplugin.mycommand.MyCommand;
+
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyPlugin extends JavaPlugin {
@@ -10,7 +13,8 @@ public class MyPlugin extends JavaPlugin {
 		getLogger().info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@MYPLUGIN");
 
 		this.getServer().getPluginManager().registerEvents(new PlayerEffect(this), this);
-		
+		// register command
+		this.getCommand("mp").setExecutor(new MyCommand(this));
 
 	}
 
--- MyPlugin/src/main/java/jp/mydns/dyukusi/myplugin/listener/PlayerEffect.java
+++ MyPlugin/src/main/java/jp/mydns/dyukusi/myplugin/listener/PlayerEffect.java
@@ -4,13 +4,29 @@ import jp.mydns.dyukusi.myplugin.MyPlugin;
 import jp.mydns.dyukusi.myplugin.task.FireProtection;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockGrowEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.world.StructureGrowEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -31,24 +47,116 @@ public class PlayerEffect implements Listener {
 	void SplashPotion(PotionSplashEvent event) {
 
 		if (event.getEntity() instanceof Player) {
-			new FireProtection(plugin, (Player) event.getEntity()).runTaskLater(plugin, 5);
+			new FireProtection(plugin, (Player) event.getEntity())
+					.runTaskLater(plugin, 5);
+		}
+	}
+
+	@EventHandler
+	void EntityDamage(EntityDamageEvent event) {
+		if (event.getEntityType().equals(EntityType.VILLAGER)
+				&& event.getCause().equals(DamageCause.ENTITY_ATTACK)) {
+			event.setCancelled(true);
 		}
 	}
 
 	@EventHandler
 	void CraftItem(CraftItemEvent event) {
 
-		plugin.getServer().broadcastMessage(ChatColor.GREEN + "item name : " + event.getCurrentItem().getType());
-
 		Material type = event.getCurrentItem().getType();
-		
-		if(type.equals(Material.STONE_SWORD)){
+
+		if (type.equals(Material.STONE_SWORD)) {
 			ItemStack item = event.getCurrentItem();
 			ItemMeta meta = item.getItemMeta();
-			
-			
+
 		}
-		
 	}
+
+	// 海底神殿用
+	@EventHandler
+	void MonsterSpawn(CreatureSpawnEvent event) {
+
+		if (event.getEntityType().equals(EntityType.GUARDIAN)) {
+			if (event.isCancelled()) {
+				event.setCancelled(false);
+			}
+		}
+	}
+
+	// y30以下松明禁止,着火禁止
+	@EventHandler(priority = EventPriority.HIGHEST)
+	void BlockPlace(BlockPlaceEvent event) {
+		Block block = event.getBlock();
+		Material material = block.getType();
+
+		// 松明
+		if (material.equals(Material.TORCH)
+				&& block.getLocation().getBlockY() <= 30) {
+			block.setType(Material.AIR);
+			Item item = block.getWorld().dropItemNaturally(block.getLocation(),
+					new ItemStack(Material.STICK));
+			block.getWorld().playSound(block.getLocation(), Sound.FIZZ, 0.8F,
+					1.5F);
+			event.getPlayer().sendMessage(
+					ChatColor.RED + "空気が薄すぎる！" + ChatColor.AQUA
+							+ " < Lack of oxygen! >");
+		}
+		// ジャック・オ・ランタン
+		else if (material.equals(Material.JACK_O_LANTERN)
+				&& block.getLocation().getBlockY() <= 30) {
+			block.setType(Material.PUMPKIN);
+			block.getWorld().playSound(block.getLocation(), Sound.FIZZ, 0.8F,
+					1.5F);
+			event.getPlayer().sendMessage(
+					ChatColor.RED + "空気が薄すぎる！" + ChatColor.AQUA
+							+ " < Lack of oxygen! >");
+		}
+
+	}
+
+	// y30以下ではネザーラックに着火禁止
+	@EventHandler(priority = EventPriority.HIGHEST)
+	void Ignite(BlockIgniteEvent event) {
+		Block block = event.getBlock().getWorld()
+				.getBlockAt(event.getBlock().getLocation().add(0, -1, 0));
+		Location location = block.getLocation();
+
+		if (event.getCause().equals(IgniteCause.FLINT_AND_STEEL)) {
+
+			if (block.getType().equals(Material.NETHERRACK)) {
+
+				if (location.getBlockY() <= 30) {
+					event.setCancelled(true);
+					event.getPlayer().sendMessage(
+							ChatColor.RED + "空気が薄すぎる！" + ChatColor.AQUA
+									+ " < Lack of oxygen! >");
+
+				}
+			}
+		}
+
+	}
+
+	//
+	// @EventHandler
+	// void BlockGrow(BlockGrowEvent event) {
+	// Block block = event.getBlock();
+	//
+	// }
+	//
+	// @EventHandler
+	// void CanBuild(BlockCanBuildEvent event) {
+	// plugin.getServer().broadcastMessage(
+	// event.getBlock().getType() + "が設置するよ");
+	//
+	// }
+	//
+	// @EventHandler
+	// void StructureGrow(StructureGrowEvent event) {
+	// for (BlockState blockstate : event.getBlocks()) {
+	// plugin.getServer().broadcastMessage(
+	// blockstate.toString() + "が成長(structure)");
+	// }
+	// }
 
 }
